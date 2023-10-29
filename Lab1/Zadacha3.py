a266b8f1ffac75665e9ae3a3f1dcf389ae910cc1
TemirkhanMussayev <mussayevtk@gmail.com>
2023-10-29 17:55:59 +0600

    zadacha 3

---
+++ Lab1/Zadacha3.py
@@ -0,0 +1,7 @@
+a = int(input("Vvedite pervoe chislo: "))
+b = int(input("vvedite vtoroe chislo: "))
+
+if a > b:
+    print("naibolwee chislo eto: ", a)
+else:
+    print("naibolwee chislo eto: ", b)
